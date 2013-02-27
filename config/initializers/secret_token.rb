08f3cc019ba2dbffbc9c7ade7f3f5065905da4db
Hiro White <hiro@agoristradio.com>
2013-02-26 19:48:02 -0600

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Trunames::Application.config.secret_token = '710c938f4f3593b88937cae1ea170cca323500e6dd59eab1234eb5b2515492efee02789600f08f551fefa746bcb2d1916dde765cbbb27e18ef8481421a483d86'
